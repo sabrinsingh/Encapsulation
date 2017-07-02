2b9cfa69035e04e34de23100b0e50b61092d0722
Sabrin <sabrinlalsingh@gmail.com>
2017-07-02 17:38:23 +0545

    GETTER AND SETTER project is added

---
+++ GetterSetter/src/Account.java
@@ -0,0 +1,31 @@
+
+public class Account {
+  private String name;
+  private String number;
+  private double amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+  
+}
---
+++ GetterSetter/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+    
+    public static void main(String[] args) {
+        Account act = new Account();
+        act.setAmount(10000);
+        act.setName("Sudeep");
+        act.setNumber("0918837734349034");
+        System.out.println("Account Name: "+act.getName());
+    }
+    
+}
